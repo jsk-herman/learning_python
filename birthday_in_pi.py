9053075bdc7d5332d4e94987f27926640e7e5458
Je Sian Keith Herman <jeseankeith.hisman@gmail.com>
2020-11-23 18:38:01 +0800

    Remove string limit of 6 char

--- birthday_in_pi.py
+++ birthday_in_pi.py
@@ -1,20 +1,17 @@
-# This script finds your birthday in MMDDYY format among the digits of pi.
-# Actually, the script accepts any numerical string as long as it is 6 digits long.
-# So you can probably use any 6-digit combination with it.
+# This script finds your birthday in number format among the digits of pi.
+# Actually, the script accepts any numerical string.
+# So you can probably use any numerical sequence with it.
 
 file = open('pi_1m.txt', 'r')                                                            # Opens the file containing the number pi
-pi_1m = file.read()                                                                      # Reads the file and stores the string to a variable
-birthday = input("What is your birthday in the MMDDYY format? ")                         # Asks for the user for their birthday in MMDDYY format
+pi_digits = file.read()                                                                  # Reads the file and stores the string to a variable
+birthday = input("What sequence of digits would you like to find in pi? ")               # Asks for the user for their number sequence
 try:
     int(birthday)                                                                        # Checks to see if the input is a numerical string
-    length = len(birthday)                                                               # Determines the length of the string
 except:
-    print("Please input your birthday according to the format and try again.\n")         # Outputs an error message if the input is not according to the format because it failed the int() test
-if length ==  6:                                                                         # Checks to see if the input is 6 digits long.
-    pos = int(pi_1m.find(birthday))                                                      # Finds the 6-digit combination in pi using the find() method
-    if pos > 0:
-        print("Your birthday was first found at digit " + str(pos) + " of pi.\n")        # If pos is not -1 then print a message of the 6-digit comnbination's position in pi
-    else:
-        print("Your birthday was not found in the first million digits of pi.\n")        # If pos is -1 then print an error message saying it was not found.
+    print("Please input a sequence of digits and try again.\n")                          # Outputs an error message if the input is not according to the format because it failed the int() test
+pos = pi_digits.find(birthday)                                                           # Finds the number sequence in pi using the find() method
+pos = pos + 1                                                                            # Accounts for python numbering and change to the ordinal system
+if pos > 0:
+    print("Your sequence was first found at digit " + str(pos) + " of pi.\n")            # If pos is not -1 then print a message of the number sequence's position in pi
 else:
-    print("Please input your birthday according to the format and try again.\n")         # If the input is not 6 digits long just like the format then print an error message.
+    print("Your sequence was not found in the first million digits of pi.\n")            # If pos is -1 then print an error message saying it was not found.
